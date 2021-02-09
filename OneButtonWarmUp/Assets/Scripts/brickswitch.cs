68783b8440eed1b6dd02148c5d0726a4bd76cedc
Al <theonlypurplecat@gmail.com>
2021-02-09 15:21:19 +1300

    Small fixes

--- OneButtonWarmUp/Assets/Scripts/brickswitch.cs
+++ OneButtonWarmUp/Assets/Scripts/brickswitch.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class brickswitch : MonoBehaviour
 {
+
+    //GameObjects
+    [SerializeField]
+    private GameObject DuckUnder;
+    [SerializeField]
+    private GameObject GoThrough;
+
+
+    [SerializeField]
+    private bool under;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      //  StartCoroutine(Switchflip());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+
+
+
+
+
+
     }
+
+
+    //IEnumerator Switchflip()
+   // {
+        
+
+
+    //}
+
+
+    void SetUpright()
+    {
+        under = false;
+        DuckUnder.SetActive(false);
+        GoThrough.SetActive(true);
+    }
+
+    void SetUnder()
+    {
+        under = true;
+        DuckUnder.SetActive(true);
+        GoThrough.SetActive(false);
+    }
+
 }
